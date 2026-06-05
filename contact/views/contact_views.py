52948a25c2cb964f20b34c9a91d80572d384dc0a
Alessandro Moraes <alesmoraes@gmail.com>
2026-06-05 13:26:24 -0400

    Usando a request no template para pegar o valor de GET

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -38,7 +38,8 @@ def search(request):
 
     context = {
         'contacts': contacts,
-        'site_title': 'Contatos -'
+        'site_title': 'Search -',
+        'search_value': search_value
     }
 
     return render(
