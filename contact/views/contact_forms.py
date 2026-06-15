a2dd5d10207985985258c59a36d14247c670770d
Alessandro Moraes <alesmoraes@gmail.com>
2026-06-15 17:28:53 -0400

    Configurando os campos e widgets do formulário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,31 +1,5 @@
-from typing import Any
-
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from contact.models import Contact
-from django.core.paginator import Paginator
-from django.core.exceptions import ValidationError
-
-from django import forms
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone')
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de Erro',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def create(request):
