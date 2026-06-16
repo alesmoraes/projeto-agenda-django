34ac6ba5750b5df1a77de7bb9a77fb530fede62b
Alessandro Moraes <alesmoraes@gmail.com>
2026-06-16 17:40:14 -0400

    Adicionando mais campos no formulário de contato

--- contact/forms.py
+++ contact/forms.py
@@ -6,7 +6,9 @@ from django.core.exceptions import ValidationError
 class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = (
+            'first_name', 'last_name', 'phone', 'email', 'description',
+            'category')
 
     def clean(self):
         cleaned_data = self.cleaned_data
